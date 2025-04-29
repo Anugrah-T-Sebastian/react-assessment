f075be64601eb839eb1d142d530437ef52bacd31
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-04-30 00:53:36 +0530

    Custom hook created to handle password generation

--- 04-password-generator/src/App.js
+++ 04-password-generator/src/App.js
@@ -1,36 +1,62 @@
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/use-password-generator";
 import "./styles.css";
 
 export default function App() {
   // HOOKS
+  const [length, setLength] = useState(4);
   const [checkboxData, setCheckboxData] = useState([
-    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Uppercase Letters", state: true },
     { title: "Include Lowercase Letters", state: false },
     { title: "Include Numbers", state: false },
     { title: "Include Symbols", state: false },
   ]);
 
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+
+  // FUNCTION
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+
   // RENDER
   return (
     <div className="container">
       {/* Password Text and Copy */}
-      <div className="header">
-        <div className="title">Hjsddas</div>
-        <button className="copyBtn" onClick={() => {}}></button>
-      </div>
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <button className="copyBtn" onClick={() => {}}>
+            copy
+          </button>
+        </div>
+      )}
       {/* Character length */}
       <div className="charlength">
         <span>
           <label>Character length</label>
-          <label>4</label>
+          <label>{length}</label>
         </span>
-        <input type="range" min="4" max="20" onChange={() => {}} />
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
       </div>
       {/* Checkboxes */}
       <div className="checkboxes">
         {checkboxData.map((checkbox, i) => {
           return (
             <div key={i}>
-              <input type="checkbox" checked={checkbox.state} />
+              <input
+                type="checkbox"
+                checked={checkbox.state}
+                onChange={() => handleCheckboxChange(i)}
+              />
               <label>{checkbox.title}</label>
             </div>
           );
@@ -38,7 +64,10 @@ export default function App() {
       </div>
       {/* Strength */}
       {/* Generate Button */}
-      <button className="generateBtn" onChange={() => {}}>
+      <button
+        className="generateBtn"
+        onClick={() => generatePassword(checkboxData, length)}
+      >
         Generate Password
       </button>
     </div>
